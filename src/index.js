9a505498f1640b1e5734f31373e5bab908b58052
amelur <ameluckin@gmail.com>
2023-08-18 16:23:44 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const result = [];
+
+  for (const char of str) {
+    for (let [open, close] of bracketsConfig) {
+      if (open.includes(char)) {
+        const correspondingClosing = close[open.indexOf(char)];
+        if (
+          char === correspondingClosing &&
+          result.length > 0 &&
+          result[result.length - 1] === char
+        ) {
+          result.pop();
+        } else {
+          result.push(char);
+        }
+      } else if (close.includes(char)) {
+        const correspondingOpening = open[close.indexOf(char)];
+        if (!result.length || result.pop() !== correspondingOpening) {
+          return false;
+        }
+      }
+    }
+  }
+
+  return result.length === 0;
+};
